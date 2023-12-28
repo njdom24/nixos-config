57250aea7ab508cd2c1038041f8df3ec5f1a7237
Damino <dom32400@aol.com>
2023-12-28 14:59:07 -0500

    Fix non-headless env

--- hosts/common/desktops/sway/default.nix
+++ hosts/common/desktops/sway/default.nix
@@ -88,10 +88,9 @@ in
     export XDG_CURRENT_DESKTOP=sway
     export NIXOS_OZONE_WL=1
 
-    REMOTE_ENABLED=$(pgrep -x x11vnc > /dev/null && echo 1 || echo 0)
+    export REMOTE_ENABLED=$(pgrep -x x11vnc > /dev/null && echo 1 || echo 0)
     export WLR_NO_HARDWARE_CURSORS=$REMOTE_ENABLED
-    export WLR_BACKENDS=$([ $REMOTE_ENABLED -eq 0 ] && echo "headless,libinput" || echo "")
-    
+    export WLR_BACKENDS=$([ $REMOTE_ENABLED = 1 ] && echo "headless,libinput" || echo "drm,libinput")
     
     eval $(gnome-keyring-daemon --start --daemonize --components=pkcs11,secrets,ssh)
     export SSH_AUTH_SOCK
