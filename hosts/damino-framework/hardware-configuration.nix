aa3d5a6060ce4cfe2eb4b0633d786b6d1a4403be
Damino <dom32400@aol.com>
2024-02-20 21:28:35 -0500

    Revert to LTS kernel - 6.7 has freezing bug

--- hosts/damino-framework/hardware-configuration.nix
+++ hosts/damino-framework/hardware-configuration.nix
@@ -13,7 +13,7 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.kernelParams = [ "amdgpu.pcie_gen_cap=0x4000" "hid_apple.fnmode=2" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
   fileSystems."/" = {
       device = "/dev/disk/by-uuid/a4fa4d1f-24cc-45b6-890d-2526c32e922b";
