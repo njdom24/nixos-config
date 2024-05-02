bfe2db59ca0776108bc59e57e32d15733fd81c22
Damino <dom32400@aol.com>
2024-05-02 19:02:28 -0400

    Add samrewritten, use unstable Lutris

--- hosts/common/users/damino/default.nix
+++ hosts/common/users/damino/default.nix
@@ -1,3 +1,4 @@
+
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -66,8 +67,9 @@ in
       vlc
       steam-run
       steamtinkerlaunch
+      samrewritten
       moonlight-qt
-      lutris
+      unstable.lutris
       vkbasalt
       protontricks
       protonup-qt
