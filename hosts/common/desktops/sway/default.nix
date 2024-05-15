a9fc53a79b5be20898cc7cb48782a56712c987b9
Damino <dom32400@aol.com>
2024-05-14 22:38:40 -0400

    Add X fallback to Wayland envs, run Clutter on Wayland

--- hosts/common/desktops/sway/default.nix
+++ hosts/common/desktops/sway/default.nix
@@ -82,8 +82,10 @@ in
 
 	
     extraSessionCommands = ''
-    export SDL_VIDEODRIVER=wayland
-    export QT_QPA_PLATFORM=wayland
+    #export SDL_VIDEODRIVER=wayland
+    export QT_QPA_PLATFORM=wayland;xcb
+    export GDK_BACKEND=wayland,x11
+    export CLUTTER_BACKEND=wayland
     export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
     export _JAVA_AWT_WM_NONREPARENTING=1
     export MOZ_ENABLE_WAYLAND=1
