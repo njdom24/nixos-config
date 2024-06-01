a0384428bc265afd5922eec299f76da229e9aaf2
Damino <dom32400@aol.com>
2024-06-01 00:22:38 -0400

    plasma5 -> plasma6

--- hosts/damino-desktop/default.nix
+++ hosts/damino-desktop/default.nix
@@ -57,7 +57,7 @@
   #services.xserver.displayManager.gdm.enable = true;
   # Enable the KDE Plasma Desktop Environment.
   #services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
--- hosts/damino-framework/default.nix
+++ hosts/damino-framework/default.nix
@@ -80,7 +80,7 @@
   #services.xserver.displayManager.gdm.enable = true;
   # Enable the KDE Plasma Desktop Environment.
   #services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
   #services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
