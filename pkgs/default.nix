9016a27e49b0c10c57a3077e154031722ac8b360
Damino <dom32400@aol.com>
2024-09-15 21:41:47 -0400

    Derive Citra from Lime3DS

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,21 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-{pkgs, ...}: {
+{ pkgs, ...}: {
   # example = pkgs.callPackage ./example { };
   xcursor-pro = pkgs.callPackage ./xcursor-pro { };
-  citra-mk7 = pkgs.callPackage ./citra { branch = "mk7"; };
+  citra-mk7 = pkgs.unstable.lime3ds.overrideAttrs (final: prev: {
+    pname = "citra";
+    version = "r5115f64"; # Git release version
+
+    src = pkgs.fetchFromGitHub {
+      owner = "PabloMK7";
+      repo = "citra";
+      rev = "${final.version}";
+      sha256 = "sha256-7l32wBJLunp/wjUc02uiXS8XRkJkm+ckLUQqWUKIHRQ=";
+      fetchSubmodules = true;
+    };
+
+    postInstall = builtins.replaceStrings ["lime3ds"] ["citra"] ( builtins.replaceStrings ["lime3ds-cli"] ["citra"] ( builtins.replaceStrings ["lime3ds-gui"] ["citra-qt"] prev.postInstall ) );
+    meta.mainProgram = builtins.replaceStrings ["lime3ds"] ["citra"] ( builtins.replaceStrings ["lime3ds-cli"] ["citra"] ( builtins.replaceStrings ["lime3ds-gui"] ["citra-qt"] prev.meta.mainProgram ) );
+  });
 }
