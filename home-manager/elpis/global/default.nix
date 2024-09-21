90651bfce005fc07085fb64d3dc101b568e60abf
Damino <dom32400@aol.com>
2024-09-21 11:33:48 -0400

    Initial (untested) server support

---
+++ home-manager/elpis/global/default.nix
@@ -0,0 +1,112 @@
+
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  hostName,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  colorScheme = inputs.nix-colors.colorSchemes.mocha;
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      input-fonts.acceptLicense = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "elpis";
+    homeDirectory = "/home/elpis";
+    sessionPath = [ "$HOME/.local/bin" ];
+    # sessionVariables = { };
+
+    packages = with pkgs; [
+    ];
+  };
+
+  # Enable home-manager and git
+  programs = {
+    home-manager.enable = true;
+  	git = {
+  	  enable = true;
+  	  package = pkgs.gitFull;
+  	  extraConfig = {
+  	    safe.directory = "/etc/nixos/.git";
+  	  	credential.helper = "libsecret";
+  	  };
+  	  userName = "Damino";
+  	  userEmail = "dom32400@aol.com";
+  	};
+
+	zsh = {
+	  enable = true;
+	  enableCompletion = true;
+	  shellAliases = {
+	  	update = "sudo nix flake update /etc/nixos";
+	  	upgrade = "sudo nixos-rebuild switch --flake /etc/nixos/.#";
+	  	update-home = "home-manager switch --flake /etc/nixos/.";
+	  };
+	  oh-my-zsh = {
+	  	enable = true;
+	  	plugins = [ "git" ];
+	  	custom = ".oh-my-zsh";
+	  	theme = "damino";
+	  };
+	  localVariables = {
+	  	TERM = "xterm-256color"; # Fixes kitty ssh
+	  };
+	};
+  };
+
+  services = {
+  	syncthing.enable = true;
+  };
+
+  systemd.user = {
+    # Nicely reload system units when changing configs
+    startServices = "sd-switch";
+    services = {
+    };
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.11";
+}
---
+++ hosts/eitherys/default.nix
@@ -0,0 +1,240 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+let
+  gst_plugins = (with pkgs.gst_all_1; [
+  	gstreamer
+  	gst-plugins-base
+	gst-plugins-good
+	gst-plugins-bad
+	gst-plugins-ugly
+	gst-libav
+	gst-vaapi
+  ]);
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "eitherys"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking = {
+  	dhcpcd.enable = true;
+  	wireless.iwd.enable = false;
+  	interfaces.enp4s0.wakeOnLan.enable = true;
+  };
+
+  # Set your time zone.
+  time = {
+  	timeZone = "America/New_York";
+  	# hardwareClockInLocalTime = true;
+  };
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services = {
+    resolved.enable = true;
+
+    openssh = {
+      enable = true;
+      settings = {
+      	X11Forwarding = true;
+      	PasswordAuthentication = true;
+      };
+    };
+
+    gnome.gnome-keyring.enable = true;
+    gvfs.enable = true;
+
+    fstrim.enable = true;
+
+    apcupsd = {
+      enable = true;
+      configText = ''
+        UPSTYPE usb
+        NISIP 127.0.0.1
+        ONBATTERYDELAY 6
+        BATTERYLEVEL 10
+        MINUTES 3
+        TIMEOUT 0
+        ANNOY 300
+        ANNOYDELAY 60
+        BEEPSTATE T
+      '';
+    };
+  };
+
+  hardware = {
+  	#opengl.extraPackages = with pkgs; [ vpl-gpu-rt ]; #  24.10+
+  	opengl.extraPackages = with pkgs; [ onevpl-intel-gpu ];
+  	intel-gpu-tools.enable = true;
+  };
+  security.rtkit.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.elpis = {
+    isNormalUser = true;
+    description = "elpis";
+    extraGroups = [ 
+    	"networkmanager"
+    	"wheel"
+    	"video"
+    	"audio"
+    	"render"
+    	"input"
+    	"kvm"
+    ] ++ ifTheyExist [
+    	"docker"
+    	"libvirtd"
+    	"plugdev"
+    ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      home-manager
+      mesa-demos
+      vulkan-tools
+      nvtopPackages.full
+      rclone
+      handbrake
+      firefox
+      (chromium.override { enableWideVine = true; })
+      kitty
+      ffmpeg-full
+      libva-utils
+      steam-run
+      unstable.lutris
+      fastfetch
+      zsh
+      oh-my-zsh
+      mission-center
+      iwgtk
+      fd
+      btop
+      killall
+	  nix-index
+	  pavucontrol
+	  pulseaudio # Needed for pactl
+	  remmina
+	  filezilla
+	  blueberry
+	  cinnamon.warpinator
+	  gnome.gnome-disk-utility
+	  gnome.gnome-system-monitor
+	  libnotify
+	  xwaylandvideobridge
+	  xorg.xeyes
+	  corefonts
+	  vistafonts
+    ];
+  };
+
+
+  users.users.jellyfin = {
+  	isSystemUser = true;
+  	description = "jellyfin rffmpeg";
+  	uid = 965;
+  	group = "jellyfin";
+  	createHome = true;
+  	home = "/home/jellyfin";
+  	shell = pkgs.bash;
+  	extraGroups = [
+  	  "video"
+  	  "audio"
+  	  "render"
+  	];
+  	packages = with pkgs; [ steam-run jellyfin-ffmpeg ];
+  };
+
+  users.groups.jellyfin = {};
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    micro
+    x11vnc
+    wayvnc
+    lsof
+    file
+    sshfs
+    libarchive
+    p7zip
+    unzip
+    duperemove
+    xdotool
+    ecryptfs
+    ethtool
+    wineWowPackages.waylandFull
+    winetricks
+    pciutils
+    libgcc
+    bison
+    flex
+    OVMFFull
+    python3
+    distrobox
+    waypipe
+  ] ++ gst_plugins;
+
+  variables = {
+  	"GST_PLUGIN_SYSTEM_PATH_1_0" = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" gst_plugins;
+  };
+
+  extraInit = "source ${config.users.users.elpis.home}/.nix-profile/etc/profile.d/hm-session-vars.sh";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  networking.firewall = {
+  	allowedTCPPorts = [
+  	];
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
