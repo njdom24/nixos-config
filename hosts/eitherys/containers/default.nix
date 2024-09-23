382e3e5f098857687f1edbb5e8acf634d6d1b76d
Damino <dom32400@aol.com>
2024-09-23 09:40:07 -0400

    Add var substitution option to containers

--- hosts/eitherys/containers/default.nix
+++ hosts/eitherys/containers/default.nix
@@ -1,12 +1,30 @@
-{ config, pkgs, ... }:
+{ lib, pkgs, ... }:
 
+# Define the function to substitute strings in a YAML file (no default args)
 let
+  substituteYaml = { file, searchStrings, replaceStrings }: 
+    pkgs.writeTextFile {
+      name = "${builtins.baseNameOf file}-substituted.yaml";  # Create a new name based on the original file
+      text = builtins.replaceStrings searchStrings replaceStrings (builtins.readFile file);
+    };
+
   composeServices = [
+    # If file requires replacements
+    #{
+    #  name = "flaresolverr";
+    #  file = substituteYaml {
+    #    file = ./flaresolverr.yaml;
+    #    searchStrings = [ "OLD_STRING" ];   # Define the strings to replace
+    #    replaceStrings = [ "NEW_STRING" ];  # Define the replacements
+    #  };
+    #}
+
     { name = "flaresolverr"; file = ./flaresolverr.yaml; }
     { name = "suwayomi"; file = ./suwayomi.yaml; }
   ];
 in
 {
+  # Generate systemd services for each docker-compose file
   systemd.services = builtins.listToAttrs (map (service: {
     name = service.name;
     value = {
