787a84c9cb3bc070a25424723d4c23e4cb5835d6
Damino <dom32400@aol.com>
2024-09-23 10:29:43 -0400

    Avoid redundant name specification

--- hosts/eitherys/containers/default.nix
+++ hosts/eitherys/containers/default.nix
@@ -8,39 +8,57 @@ let
       text = builtins.replaceStrings searchStrings replaceStrings (builtins.readFile file);
     };
 
+  # Function to determine the appropriate file for Exec commands
+  getFileForExec = service: 
+    if (service ? searchStrings && service ? replaceStrings)
+      then substituteYaml { file = service.file; searchStrings = service.searchStrings; replaceStrings = service.replaceStrings; }
+      else service.file;
+
+  # Function to get the name without any extension
+  nameWithoutExtension = file: let
+    baseName = builtins.baseNameOf file;
+    parts = lib.strings.splitString "." baseName; # Use lib.strings.splitString
+  in
+    if builtins.length parts > 1
+      then builtins.concatStringsSep "." (lib.lists.take (builtins.length parts - 1) parts)
+      else baseName; # Return the base name if no extension
+
+  # Define the services
   composeServices = [
     # If file requires replacements
     #{
-    #  name = "flaresolverr";
-    #  file = substituteYaml {
-    #    file = ./flaresolverr.yaml;
-    #    searchStrings = [ "OLD_STRING" ];   # Define the strings to replace
-    #    replaceStrings = [ "NEW_STRING" ];  # Define the replacements
-    #  };
+    #  file = ./flaresolverr.yaml;
+    #  searchStrings = [ "OLD_STRING" ];   # Define the strings to replace
+    #  replaceStrings = [ "NEW_STRING" ];  # Define the replacements
     #}
 
-    { name = "flaresolverr"; file = ./flaresolverr.yaml; }
-    { name = "suwayomi"; file = ./suwayomi.yaml; }
+    { file = ./flaresolverr.yaml; }
+    { file = ./suwayomi.yaml; }
   ];
 in
 {
   # Generate systemd services for each docker-compose file
-  systemd.services = builtins.listToAttrs (map (service: {
-    name = service.name;
-    value = {
-      description = "Docker Compose ${service.name}";
-      after = [ "docker.service" ];
-      wants = [ "docker.service" ];
-      serviceConfig = {
-        WorkingDirectory = "/srv/docker/${service.name}";
-        TimeoutStartSec = "60min";
-        ExecStartPre = "${pkgs.docker}/bin/docker compose -f ${service.file} pull";
-        ExecStart = "${pkgs.docker}/bin/docker compose -f ${service.file} up";
-        ExecStop = "${pkgs.docker}/bin/docker compose -f ${service.file} down";
-        Restart = "on-failure";
-        Type = "simple";
+  systemd.services = builtins.listToAttrs (map (service:
+  let
+    serviceName = nameWithoutExtension service.file;
+  in
+    {
+      name = serviceName;  # Use the derived name
+      value = {
+        description = "Docker Compose ${serviceName}";
+        after = [ "docker.service" ];
+        wants = [ "docker.service" ];
+        serviceConfig = {
+          WorkingDirectory = "/srv/docker/${serviceName}";
+          TimeoutStartSec = "60min";
+          ExecStartPre = "${pkgs.docker}/bin/docker compose -f ${getFileForExec service} pull";
+          ExecStart = "${pkgs.docker}/bin/docker compose -f ${getFileForExec service} up";
+          ExecStop = "${pkgs.docker}/bin/docker compose -f ${getFileForExec service} down";
+          Restart = "on-failure";
+          Type = "simple";
+        };
+        wantedBy = [ "multi-user.target" ];
       };
-      wantedBy = [ "multi-user.target" ];
-    };
-  }) composeServices);
+    }
+  ) composeServices);
 }
