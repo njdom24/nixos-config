18528402f5272e7b7c46ac4fb72ce7023e0d7f67
Damino <dom32400@aol.com>
2024-09-25 11:25:10 -0400

    Fix concat for multiple service/timers, add SMART

--- hosts/eitherys/storage.nix
+++ hosts/eitherys/storage.nix
@@ -11,34 +11,58 @@ in
 
   services.smartd = {
     enable = true;
-  	devices = [
-  	  { device = "/dev/disk/by-id/usb-WD_My_Passport_2626_575839324435334152364A43-0:0"; }	
-  	];
+    devices = [
+      { device = "/dev/disk/by-id/usb-WD_My_Passport_2626_575839324435334152364A43-0:0"; } 
+    ];
   };
 
-  # Define everything under the systemd block
   systemd = {
-    services = lib.mkMerge (map (device: {
-      "btrfs-scrub-${lib.replaceStrings ["/"] ["_"] device}" = {
-        description = "Btrfs Scrub Monitoring Service for ${device}";
-        after = [ "network.target" ];
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = [ "${pkgs.btrfs-progs}/bin/btrfs scrub start -B ${device}" ];
+    services = lib.mkMerge (lib.concatMap (device: [
+      {
+        "btrfs-scrub-${lib.replaceStrings ["/"] ["_"] device}" = {
+          description = "Btrfs Scrub Monitoring Service for ${device}";
+          after = [ "network.target" ];
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = [ "${pkgs.btrfs-progs}/bin/btrfs scrub start -B ${device}" ];
+          };
+          restartIfChanged = true;
         };
-        restartIfChanged = true;
-      };
-    }) btrfsDevices);
+      }
+    ]) btrfsDevices ++ lib.concatMap (device: [
+      {
+        "smart-monitor-${lib.replaceStrings ["/"] ["_"] device}" = {
+          description = "SMART Monitoring Service for Extended Test on ${device}";
+          after = [ "network.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = [ "${pkgs.smartmontools}/bin/smartctl -t long ${device}" ];
+          };
+          restartIfChanged = true;
+        };
+      }
+    ]) smartDevices);
 
-    timers = lib.mkMerge (map (device: {
-      "btrfs-scrub-${lib.replaceStrings ["/"] ["_"] device}.timer" = {
-        description = "Monthly Btrfs Scrub Timer for ${device}";
-        timerConfig.OnCalendar = "*-*-01 01:00:00";  # Runs at 1:00 AM on the first of each month
-        wantedBy = [ "timers.target" ];
-        timerConfig.Persistent = true;
-        # Link the timer to the service
-        timerConfig.Unit = "btrfs-scrub-${lib.replaceStrings ["/"] ["_"] device}.service";  # Use the modified device name
-      };
-    }) btrfsDevices);
+    timers = lib.mkMerge (lib.concatMap (device: [
+      {
+        "btrfs-scrub-${lib.replaceStrings ["/"] ["_"] device}.timer" = {
+          description = "Monthly Btrfs Scrub Timer for ${device}";
+          timerConfig.OnCalendar = "*-*-01 01:00:00";  # Runs at 1:00 AM on the first of each month
+          wantedBy = [ "timers.target" ];
+          timerConfig.Persistent = true;
+          timerConfig.Unit = "btrfs-scrub-${lib.replaceStrings ["/"] ["_"] device}.service";
+        };
+      }
+    ]) btrfsDevices ++ lib.concatMap (device: [
+      {
+        "smart-monitor-${lib.replaceStrings ["/"] ["_"] device}.timer" = {
+          description = "Monthly SMART Test Timer for ${device}";
+          timerConfig.OnCalendar = "*-*-01 04:00:00";  # Runs at 4:00 AM on the first of each month
+          wantedBy = [ "timers.target" ];
+          timerConfig.Persistent = true;
+          timerConfig.Unit = "smart-monitor-${lib.replaceStrings ["/"] ["_"] device}.service";
+        };
+      }
+    ]) smartDevices);
   };
 }
