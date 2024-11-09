46547b33eadd6e4f830c42fa006a407fa40c3c55
Damino <dom32400@aol.com>
2024-11-09 01:29:43 -0500

    Early OOM for nix builds

--- hosts/common/users/damino/default.nix
+++ hosts/common/users/damino/default.nix
@@ -320,6 +320,21 @@ in
   	tmp.useTmpfs = true;
   };
 
+  # OOM configuration: https://discourse.nixos.org/t/nix-build-ate-my-ram/35752
+  systemd = {
+    # Create a separate slice for nix-daemon that is
+    # memory-managed by the userspace systemd-oomd killer
+    slices."nix-daemon".sliceConfig = {
+      ManagedOOMMemoryPressure = "kill";
+      ManagedOOMMemoryPressureLimit = "50%";
+    };
+    services."nix-daemon".serviceConfig.Slice = "nix-daemon.slice";
+
+    # If a kernel-level OOM event does occur anyway,
+    # strongly prefer killing nix-daemon child processes
+    services."nix-daemon".serviceConfig.OOMScoreAdjust = 1000;
+  };
+
   environment = {
   	systemPackages = with pkgs; [
   	  lsof
