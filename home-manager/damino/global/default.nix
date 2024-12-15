1cb8fefaac4cc9ec67e0543982bc460e8b4fcf8b
Damino <dom32400@aol.com>
2024-12-15 12:12:30 -0500

    Add comment

--- home-manager/damino/global/default.nix
+++ home-manager/damino/global/default.nix
@@ -76,7 +76,7 @@
       MESA_VK_WSI_PRESENT_MODE = "fifo"; # MangoHud vsync is non-functional as of 24.11; https://gitlab.freedesktop.org/mesa/mesa/-/issues/11379
       RADV_PERFTEST = "nggc"; # https://www.phoronix.com/news/Mesa-24.3-NGG-Culling-RDNA1; redundant in 25.05
       GSK_RENDERER = "ngl"; # https://bbs.archlinux.org/viewtopic.php?id=299488; probably fixed by 25.05
-      LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.xorg.libX11}/lib"; # Fixed MangoHud for Wayland apps: https://github.com/flightlessmango/MangoHud/issues/1497
+      LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.xorg.libX11}/lib"; # Fixed MangoHud for Wayland apps: https://github.com/ValveSoftware/gamescope/pull/1666 https://github.com/flightlessmango/MangoHud/issues/1497
     };
 
     file = {
