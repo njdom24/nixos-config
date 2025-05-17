16c5654599027d495481e017d4b5ed9eb9b50987
Damino <dom32400@aol.com>
2025-05-17 00:53:44 -0400

    Use temp home-manager stable file until 25.05

---
+++ home-manager/common/zsh/stable.nix
@@ -0,0 +1,29 @@
+{ inputs, config, pkgs, ... }: {
+  programs.zsh = {
+	enable = true;
+	enableCompletion = true;
+	shellAliases = {
+	  update = "sudo nix flake update --flake /etc/nixos/";
+	  #upgrade = "sudo nixos-rebuild switch --flake /etc/nixos/.#";
+	  #hm-upgrade = "home-manager switch --flake /etc/nixos/.";
+	};
+	initExtra = ''
+	  upgrade() {
+	    sudo nixos-rebuild switch --flake /etc/nixos/.# "$@"
+	  }
+	  hm-upgrade() {
+	    home-manager switch --flake /etc/nixos/. "$@"
+	  }
+	'';
+	oh-my-zsh = {
+	 enable = true;
+	 plugins = [ "git" ];
+	 extraConfig = ''
+	   source ${./damino.zsh-theme}
+	 '';
+	};
+	localVariables = {
+	  TERM = "xterm-256color"; # Fixes kitty ssh
+	};
+  };
+}
--- home-manager/elpis/global/default.nix
+++ home-manager/elpis/global/default.nix
@@ -20,7 +20,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    ./../../common/zsh
+    ./../../common/zsh/stable.nix
   ];
 
   nixpkgs = {
