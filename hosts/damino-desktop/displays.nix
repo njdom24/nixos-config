af22c170e1e1e7b5503429ed6a508648f47e20b8
Damino <dom32400@aol.com>
2025-06-05 22:12:30 -0400

    Add better EDID for HDR streaming res,fps options

--- hosts/damino-desktop/displays.nix
+++ hosts/damino-desktop/displays.nix
@@ -8,13 +8,15 @@
     display = {
       outputs."HDMI-A-1".edid = "edid_qm851g.bin"; # Fix 1440p144hz, VRR to 70+ to work around judder (LFC instead), prevent blanking (48 -> 40 FRL), HDR Metadata MaxFALL 800 MaxCLL 3000 MinCLL 0.1
       outputs."HDMI-A-1".mode = "e";
-      #outputs."DP-3".edid = "edid_55r635.bin"; # For "headless" streaming through unused DP port on GPU. Add VRR range (48-120), EDID MaxFALL 686 MaxCLL 1114 MinCLL 0.1
+      #outputs."DP-3".edid = "edid_55r635.bin"; # Add VRR range (48-120), EDID MaxFALL 686 MaxCLL 1114 MinCLL 0.1
+      #outputs."DP-3".edid = "edid_g8_xiaomi_lumi.bin"; # For "headless" streaming through unused DP port on GPU. 2024 Odyssey G8 OLED (4k240) w/ Xiaomi G Pro 27i's luminance metadata
       #outputs."DP-3".mode = "e";
       edid.packages = [
         (pkgs.runCommand "custom-edid" {} ''
           mkdir -p $out/lib/firmware/edid
           cp ${./edid_qm851g.bin} $out/lib/firmware/edid/edid_qm851g.bin
           cp ${./edid_55r635.bin} $out/lib/firmware/edid/edid_55r635.bin
+          cp ${./edid_g8_xiaomi_lumi.bin} $out/lib/firmware/edid/edid_g8_xiaomi_lumi.bin
         '')
       ];
     };
