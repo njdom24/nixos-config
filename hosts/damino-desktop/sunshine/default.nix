c21b2a8c84645c810c01a975ec1fa26d5d5f0a0a
Damino <dom32400@aol.com>
2025-06-16 21:22:03 -0400

    Try to back up/restore display configs when streaming with dummy display. Also auto-set paper white, max luminance

--- hosts/damino-desktop/sunshine/default.nix
+++ hosts/damino-desktop/sunshine/default.nix
@@ -87,6 +87,17 @@
                   ;;
                 kwin_wayland)
                   echo "→ Running KDE/KWin-specific logic"
+
+                  # Back up existing display settings
+                  backup_dir="$HOME/.local/share/kscreen.bak"
+                  if [ -d "$HOME/.local/share/kscreen" ]; then
+                      rm -rf "$backup_dir"
+                      cp -r "$HOME/.local/share/kscreen" "$backup_dir"
+                      echo "Backup saved to $backup_dir"
+                  else
+                      echo "No KScreen config found to back up."
+                  fi
+                  
                   # Assume DP-3 is a dummy display used for headless
                   DUMMY="DP-3"
 
@@ -127,10 +138,20 @@
                     ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output."$DUMMY".hdr.enable
                     # https://github.com/LizardByte/Sunshine/issues/3298#issuecomment-2670218658
                     ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output."$DUMMY".colorPowerTradeoff.preferAccuracy
+                    ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output."$DUMMY".sdr-brightness.203 # Standard reference luminance
+                    ${pkgs.kdePackages.full}/bin/qdbus org.kde.Solid.PowerManagement /org/kde/Solid/PowerManagement/Actions/BrightnessControl org.kde.Solid.PowerManagement.Actions.BrightnessControl.setBrightness 10000 # Max 100.00% brightness
                   else
                     echo "Disabling HDR"
                     ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output."$DUMMY".hdr.disable
                     ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output."$DUMMY".colorPowerTradeoff.preferEfficiency
+                  fi
+
+                  if [ -d "$backup_dir" ]; then
+                      rm -rf "$HOME/.local/share/kscreen"
+                      cp -r "$backup_dir" "$HOME/.local/share/kscreen"
+                      echo "Restored from backup."
+                  else
+                      echo "No backup found at $backup_dir"
                   fi
 
                   ;;
