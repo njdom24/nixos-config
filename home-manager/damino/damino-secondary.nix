7ae754af0ea0a185c04fcd7e938fa983f895171d
Damino <dom32400@aol.com>
2025-08-15 23:18:11 -0400

    Default to kanshi for Hyprland on secondary

--- home-manager/damino/damino-secondary.nix
+++ home-manager/damino/damino-secondary.nix
@@ -4,4 +4,14 @@
   services = {
     mako.settings.output = lib.mkForce "HDMI-A-1";
   };
+
+  home.file = {
+    # Force back to default
+    ".config/hypr/hm/displays.conf" = lib.mkForce {
+      text = ''
+        monitor=,preferred,auto,auto
+        exec = timeout 10 kanshi &
+      '';
+    };
+  };
 }
