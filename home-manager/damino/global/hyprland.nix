618476e9f4a0d17813451949f23cfae7ac2ee0a4
Damino <dom32400@aol.com>
2025-08-17 01:04:10 -0400

    Add wl-clip-persist to Hyprland

--- home-manager/damino/global/hyprland.nix
+++ home-manager/damino/global/hyprland.nix
@@ -385,6 +385,7 @@
         "${set-displays}"
         "${gamescope-cursor-fix}"
         "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --daemonize --components=pkcs11,secrets,ssh)"
+        "${pkgs.wl-clip-persist}/bin/wl-clip-persist --clipboard regular --selection-size-limit 1048576 --reconnect-tries 1 --all-mime-type-regex '(?i)^(?!image/x-inkscape-svg).+'"
         "nm-applet &"
         "hyprpaper & firefox"
         # Work around cursor config option unreliability
