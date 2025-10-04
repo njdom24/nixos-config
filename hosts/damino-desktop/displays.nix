723ca9a51d36edec10284aa07abd2b250fc4e31c
Damino <dom32400@aol.com>
2025-10-04 13:06:04 -0400

    Move EDID overrides

--- hosts/damino-desktop/displays.nix
+++ hosts/damino-desktop/displays.nix
@@ -6,8 +6,6 @@
   hardware = {
     # https://github.com/NixOS/nixpkgs/pull/279789#issuecomment-2148560802
     display = {
-      #outputs."DP-1".edid = "edid_aoc_Q27G40XMN_72vrr.bin"; # Increase VRR range to 70 to work around flicker
-      #outputs."DP-1".mode = "e";
       ##outputs."HDMI-A-1".edid = "edid_qm851g.bin"; # Fix 1440p144hz, VRR to 70+ to work around judder (LFC instead), prevent blanking (48 -> 40 FRL), HDR Metadata MaxFALL 800 MaxCLL 3000 MinCLL 0.1
       outputs."HDMI-A-1".edid = "edid_q800t_xiaomi_lumi.bin"; # For "headless" streaming through unused DP port on GPU. 2024 Odyssey G8 OLED (4k240) w/ Xiaomi G Pro 27i's luminance metadata
       outputs."HDMI-A-1".mode = "e";
@@ -18,7 +16,6 @@
       edid.packages = [
         (pkgs.runCommand "custom-edid" {} ''
           mkdir -p $out/lib/firmware/edid
-          cp ${./edid_aoc_Q27G40XMN_72vrr.bin} $out/lib/firmware/edid/edid_aoc_Q27G40XMN_72vrr.bin
           cp ${./edid_qm851g.bin} $out/lib/firmware/edid/edid_qm851g.bin
           cp ${./edid_qm851g_v217.bin} $out/lib/firmware/edid/edid_qm851g_v217.bin
           cp ${./edid_55r635.bin} $out/lib/firmware/edid/edid_55r635.bin
