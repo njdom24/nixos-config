9b5b3f7a077ea5a30e9911cbd4ed650f134e8c6e
Damino <dom32400@aol.com>
2025-10-29 00:02:38 -0400

    Update comment

--- hosts/common/users/damino/gaming/gamescope.nix
+++ hosts/common/users/damino/gaming/gamescope.nix
@@ -906,7 +906,7 @@ in
         #"--backend sdl" # https://github.com/ValveSoftware/gamescope/issues/1622 and causes stutter (maybe https://github.com/ValveSoftware/gamescope/issues/995)
         "--hdr-enabled"
         "--adaptive-sync"
-        "--force-grab-cursor" # Breaks games with launchers (Elden Ring)
+        "--force-grab-cursor"
         #"-r 360" # Default that is a multiple of 120 and 180
         #"--mangoapp"
         "-F fsr"
