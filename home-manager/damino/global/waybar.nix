da49b59d389a6569130fb44a7fcb3aca5af75fec
Damino <dom32400@aol.com>
2025-11-17 00:55:48 -0500

    Use wl-gammarelay-rs instead of hyprsunset

--- home-manager/damino/global/waybar.nix
+++ home-manager/damino/global/waybar.nix
@@ -205,50 +205,27 @@
     	    #!/usr/bin/env bash
 
             # Gammastep causes stutters when gaming. Kill it to take over
-    	    kill -9 $(pgrep gammastep) 2> /dev/null
-    	    
-    	    if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
-    	      BIN="hyprsunset"
-    	      ARGS="-t 6000"
-    	    else
-    	      if ! ${pkgs.procps}/bin/pgrep -f wl-gammarelay-rs >/dev/null; then
-    	        ${pkgs.wl-gammarelay-rs}/bin/wl-gammarelay-rs &
-    	        sleep 0.5
-    	      fi
-    	      BIN="busctl"
-    	      ARGS="--user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6000"
+    	    kill -9 $(${pkgs.procps}/bin/pgrep gammastep) 2> /dev/null
+
+    	    if ! ${pkgs.procps}/bin/pgrep -f wl-gammarelay-rs >/dev/null; then
+    	      ${pkgs.wl-gammarelay-rs}/bin/wl-gammarelay-rs &
+    	      sleep 0.5
     	    fi
-    	    
+
     	    case "$1" in
     	      toggle)
-    	        if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
-    	          if ${pkgs.procps}/bin/pgrep -x "$BIN" >/dev/null; then
-    	            pkill -x "$BIN"
-    	          else
-    	            "$BIN" $ARGS &
-    	          fi
+    	        if ! ${pkgs.systemd}/bin/busctl --user get-property rs.wl-gammarelay / rs.wl.gammarelay Temperature 2>/dev/null | ${pkgs.gnugrep}/bin/grep -q '^q 6500$'; then
+    	          ${pkgs.systemd}/bin/busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500
     	        else
-    	          if ! busctl --user get-property rs.wl-gammarelay / rs.wl.gammarelay Temperature 2>/dev/null | ${pkgs.gnugrep}/bin/grep -q '^q 6500$'; then
-    	            busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500
-    	          else
-    	            "$BIN" $ARGS &
-    	          fi
+    	          ${pkgs.systemd}/bin/busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6000 &
     	        fi
     	        sleep 0.1 && pkill -RTMIN+10 waybar
     	        ;;
     	      status|*)
-    	        if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
-    	          if pgrep -x "$BIN" >/dev/null; then
-    	            echo "{\"text\":\"\",\"tooltip\":\"$BIN active\"}"
-    	          else
-    	            echo "{\"text\":\"\",\"tooltip\":\"$BIN inactive\"}"
-    	          fi
+    	        if ! ${pkgs.systemd}/bin/busctl --user get-property rs.wl-gammarelay / rs.wl.gammarelay Temperature 2>/dev/null | ${pkgs.gnugrep}/bin/grep -q '^q 6500$'; then
+    	          echo "{\"text\":\"\",\"tooltip\":\"wl-gammarelay-rs active\"}"
     	        else
-    	          if ! busctl --user get-property rs.wl-gammarelay / rs.wl.gammarelay Temperature 2>/dev/null | ${pkgs.gnugrep}/bin/grep -q '^q 6500$'; then
-    	            echo "{\"text\":\"\",\"tooltip\":\"wl-gammarelay-rs active\"}"
-    	          else
-    	            echo "{\"text\":\"\",\"tooltip\":\"wl-gammarelay-rs inactive\"}"
-    	          fi
+    	          echo "{\"text\":\"\",\"tooltip\":\"wl-gammarelay-rs inactive\"}"
     	        fi
     	        ;;
     	    esac
