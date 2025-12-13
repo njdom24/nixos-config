944dfa04cedcf50c3b236b1a970cc838e63bc8a0
Damino <dom32400@aol.com>
2025-12-12 20:18:24 -0500

    Bump Hyprland, remove merged patch

--- overlays/default.nix
+++ overlays/default.nix
@@ -54,10 +54,10 @@ in {
 
     hyprland = inputs.hyprland.packages.${prev.stdenv.hostPlatform.system}.hyprland.overrideAttrs (old: {
       patches = (old.patches or []) ++ [
-        (prev.fetchpatch {
-          url = "https://patch-diff.githubusercontent.com/raw/hyprwm/Hyprland/pull/12127.patch";
-          sha256 = "sha256-auQhLiP5cIDSYXbD/6/Im7CbeguhaZf5xe1JGCsM9O0=";
-        })
+        #(prev.fetchpatch {
+        #  url = "https://patch-diff.githubusercontent.com/raw/hyprwm/Hyprland/pull/12127.patch";
+        #  sha256 = "sha256-auQhLiP5cIDSYXbD/6/Im7CbeguhaZf5xe1JGCsM9O0=";
+        #})
       ];
     });
     xdg-desktop-portal-hyprland = inputs.hyprland.packages.${prev.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
