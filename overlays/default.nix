aa7cc7dfe070dbf24f7448001e0d74846d93c257
Damino <dom32400@aol.com>
2025-12-26 16:58:42 -0500

    Proper Gamescope modifier selection patch

--- overlays/default.nix
+++ overlays/default.nix
@@ -23,6 +23,10 @@ in {
       newPatches = [
         ../patches/gamescope-crash-fix.patch
         ../patches/gamescope-hdr-sway-fix.patch
+        (prev.fetchpatch {
+          url = "https://patch-diff.githubusercontent.com/raw/ValveSoftware/gamescope/pull/2049.patch";
+          sha256 = "sha256-g/WILRVbuJhrNsQNlQL3nYO8Lz4YdYpuaUf7jN/yYgg=";
+        })
       ];
       #addIfMissing = p: if builtins.any (x: x == p) existing then [] else [p];
     in rec {
