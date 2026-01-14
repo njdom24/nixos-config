9973d6974fcaa5716e2f0b9286e0e3ff24a1a0dd
Damino <dom32400@aol.com>
2026-01-14 01:01:22 -0500

    Fix empty Dolphin app list, more reliable HDR transition

--- home-manager/damino/global/hyprland.nix
+++ home-manager/damino/global/hyprland.nix
@@ -135,6 +135,7 @@
         "MOZ_ENABLE_WAYLAND,1"
         "MOZ_DBUS_REMOTE,1"
         "NIXOS_OZONE_WL,1"
+        "XDG_MENU_PREFIX,plasma-"
       ];
 
       #plugin = {
@@ -745,6 +746,7 @@
               fi
               echo "Enabling HDR"
               hyprctl keyword "monitorv2[$monitor]:cm" hdr
+              hyprctl keyword "monitorv2[$monitor]:bitdepth" 10
               ;;
             off)
               if [ "$current_mode" = "srgb" ]; then
