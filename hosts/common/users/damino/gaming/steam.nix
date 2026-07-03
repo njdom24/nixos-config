9e451bba69726749d962dbfe7801be30a71f187b
Damino <dom32400@aol.com>
2026-07-03 02:41:23 -0400

    Simplify Steam extest conditions

--- hosts/common/users/damino/gaming/steam.nix
+++ hosts/common/users/damino/gaming/steam.nix
@@ -178,27 +178,14 @@
         extraProfile = ''
           # https://github.com/NixOS/nixpkgs/issues/279893
           unset TZ
-          if [ -n "$SWAYSOCK" ] || [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
-            if [ "$XDG_CURRENT_DESKTOP" = "gamescope" ]; then
-              # Launched through gamescope. Could enable after https://github.com/Supreeeme/extest/issues/11 or portal issue below
-              echo "Disabling Extest"
-            else
-              # Work around Sway's refusal to unscale XWayland: https://github.com/swaywm/sway/issues/2966
-              if [[ "$XDG_CURRENT_DESKTOP" = "sway" ]]; then
-                if ! ${pkgs.procps}/bin/pgrep -f xwayland-satellite >/dev/null; then
-                  echo "xwayland-satellite is not running. Starting"
-                  ${pkgs.xwayland-satellite}/bin/xwayland-satellite &
-                  sleep 1
-                  echo "Attempting to start Steam on XWayland-Satellite"
-                  export DISPLAY=:1
-                  ${pkgs.xrdb}/bin/xrdb -merge <<< "Xft.dpi: 96"
-                fi
-              fi
-              # Needed until https://github.com/emersion/xdg-desktop-portal-wlr/issues/278
-              # and/or https://github.com/hyprwm/Hyprland/pull/7919
-              export LD_PRELOAD="$LD_PRELOAD:${pkgs.pkgsi686Linux.extest}/lib/libextest.so"
-              echo "Enabling Extest"
-            fi
+          if [ "$XDG_CURRENT_DESKTOP" = "gamescope" ]; then
+            # Launched through gamescope. Could enable after https://github.com/Supreeeme/extest/issues/11
+            # or portal issue: https://github.com/emersion/xdg-desktop-portal-wlr/issues/278
+            # https://github.com/hyprwm/Hyprland/pull/7919
+            echo "Disabling Extest"
+          else
+            export LD_PRELOAD="$LD_PRELOAD:${pkgs.pkgsi686Linux.extest}/lib/libextest.so"
+            echo "Enabling Extest"
           fi
 
           status="$(${pkgs.systemd}/bin/systemctl --user is-active steam-screenshot-watcher.service)"
