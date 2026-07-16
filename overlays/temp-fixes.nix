6ecd1ae288d2d8d17186f7a4010c2a1c9460be5b
Damino <dom32400@aol.com>
2026-07-15 22:34:48 -0400

    Remove temp RPCS3 fix

--- overlays/temp-fixes.nix
+++ overlays/temp-fixes.nix
@@ -1,10 +1,3 @@
 final: prev: {
-  # TODO: https://github.com/NixOS/nixpkgs/pull/530692
-  rpcs3 = prev.rpcs3.overrideAttrs (old: {
-    buildInputs = map (dep:
-      if dep == prev.glew
-      then prev.glew.override { enableEGL = false; }
-      else dep
-    ) old.buildInputs;
-  });
+
 }
