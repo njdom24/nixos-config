ab7dd8053a2717e2ee478cd78a996790ce69197b
Damino <dom32400@aol.com>
2026-09-11 00:17:29 -0400

    Launch gsr-ui under xwayland-satellite

--- home-manager/damino/global/mango/default.nix
+++ home-manager/damino/global/mango/default.nix
@@ -72,7 +72,7 @@
           exec-once=~/.config/mango/mango-spawn-on-tag.sh 1 DP-1 firefox firefox
           exec-once=~/.config/mango/mango-spawn-on-tag.sh 2 DP-2 discord env DISPLAY=:1 discord
           exec-once=~/.config/mango/mango-spawn-on-tag.sh 4 DP-1 steam env DISPLAY=:1 gtk-launch steam.desktop
-          exec-once=bash -c "sleep 5 && gsr-ui"
+          exec-once=bash -c "sleep 5 && env DISPLAY=:1 gsr-ui"
           #exec-once=discord
           #exec-once=gtk-launch steam.desktop
           
